61a6afdf787f608ff0770ff92cc716f07f908adf
lwazi-no <lwazi@kagantsa.co.za>
2021-10-24 10:09:21 +0200

    loadshedding save

---
+++ pages/layout/layout.js
@@ -0,0 +1,28 @@
+import { openModal, closeModal } from "../../src/modules/popup.js";
+
+var contentCanvas = document.querySelector(".contentCanvas");
+
+function replacecanvascontent(html_path) {
+  contentCanvas.innerHTML = `<object type="text/html" id="currentPage"  data="${html_path}" width="100%" height="100%"></object>`;
+}
+
+//  Navigation
+function gotoOverview() {
+  var link = "../law-enforcement/law-enforcement.html";
+  replacecanvascontent(link);
+}
+
+gotoOverview();
+
+var openUserMenu = () => {
+  openModal("loggedInUserPopup");
+};
+
+// open user menu
+// document.querySelector(".loggedInUser").addEventListener("click", function () {
+//   openModal("loggedInUserPopup");
+// });
+
+function logout() {
+  window.location.href = "../index.html";
+}
